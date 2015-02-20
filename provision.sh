93e3591f7150a09e0eaa75ded3300f190386ae96
Barney Hanlon <bhanlon@corto.local>
2015-02-20 07:26:18 +0000

    Switch to our epel, change digital ocean

--- provision.sh
+++ provision.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
 function provision_box() {
-  local REQUIREMENTS_ROLES_FILE='requirements.yml'
-  local INVENTORY_FILE='inventory'
-  ansible-galaxy install --force -r "${REQUIREMENTS_ROLES_FILE}"
-  ansible-playbook -i "${INVENTORY_FILE}" ./do.yml --ask-vault-pass
+  local REQUIREMENTS_PIP_FILE='./requirements.pip.txt';
+  local REQUIREMENTS_ROLES_FILE='requirements.yml';
+  local INVENTORY_FILE='inventory';
+  local PLAYBOOK_PATH='./do.yml';
+  local PROVISION_HOSTNAME='wibble'
+
+  pip install -r "${REQUIREMENTS_PIP_FILE}";
+  ansible-galaxy install --force -r "${REQUIREMENTS_ROLES_FILE}";
+  ansible-playbook -i "${INVENTORY_FILE}" \
+  --ask-vault-pass \
+  "${PLAYBOOK_PATH}" \
+  --extra-vars hostname_name="${PROVISION_HOSTNAME}"
 }
+
+provision_box
