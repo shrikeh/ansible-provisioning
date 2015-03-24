8444ac64d459e501a2c0b38a30ac0307098de748
Barney Hanlon <shrikeh@case-2.local>
2015-03-24 14:17:31 +0000

    Initial version

--- provision.sh
+++ provision.sh
@@ -4,15 +4,13 @@ function provision_box() {
   local REQUIREMENTS_PIP_FILE='./requirements.pip.txt';
   local REQUIREMENTS_ROLES_FILE='requirements.yml';
   local INVENTORY_FILE='inventory';
-  local PLAYBOOK_PATH='./do.yml';
-  local PROVISION_HOSTNAME='wibble'
+  local PLAYBOOK_PATH='./provision.yml';
 
   pip install -r "${REQUIREMENTS_PIP_FILE}";
   ansible-galaxy install --force -r "${REQUIREMENTS_ROLES_FILE}";
   ansible-playbook -i "${INVENTORY_FILE}" \
   --ask-vault-pass \
-  "${PLAYBOOK_PATH}" \
-  --extra-vars hostname_name="${PROVISION_HOSTNAME}"
+  "${PLAYBOOK_PATH}" -vvvv
 }
 
 provision_box
