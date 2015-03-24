495747f5ef0604f710441b4dcb6b55222d67b120
Barney Hanlon <shrikeh@case-2.local>
2015-03-24 14:25:09 +0000

    Update script to have more vars

--- provision.sh
+++ provision.sh
@@ -5,12 +5,17 @@ function provision_box() {
   local REQUIREMENTS_ROLES_FILE='requirements.yml';
   local INVENTORY_FILE='inventory';
   local PLAYBOOK_PATH='./provision.yml';
+  local PROVISION_HOSTNAME='radley-qa-web1';
+  local REMOTE_USER='root';
 
   pip install -r "${REQUIREMENTS_PIP_FILE}";
   ansible-galaxy install --force -r "${REQUIREMENTS_ROLES_FILE}";
   ansible-playbook -i "${INVENTORY_FILE}" \
+  -vvvv \
+  --user="${REMOTE_USER}" \
   --ask-vault-pass \
-  "${PLAYBOOK_PATH}" -vvvv
+  "${PLAYBOOK_PATH}" \
+  --extra-vars hostname_name="${PROVISION_HOSTNAME}"
 }
 
 provision_box
